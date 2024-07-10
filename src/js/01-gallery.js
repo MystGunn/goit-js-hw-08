4a23a7a7e9e6c60b4ac57dc280b9216288892b59
MystGunn <157536861+MystGunn@users.noreply.github.com>
2024-07-10 20:17:11 +0200

    Exercitiul 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// console.log(galleryItems)
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const listElement = document.querySelector(".gallery");
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+    return element
+    .map(({preview,original,description}) => {
+        return `<a class='gallery__link' href="${preview}">
+            <img class="gallery__image" src="${original}" alt="${description}"/>
+            </a>
+            `;
+    })
+    .join('');
+}
+
+listElement.insertAdjacentHTML("beforeend", photosMarkup);
+
+const galleryHandler = new SimpleLightbox('.gallery a', {
+    captionsData:'alt',
+    captionDelay:250,
+ });
+ galleryHandler.on("show.simplelightbox")
