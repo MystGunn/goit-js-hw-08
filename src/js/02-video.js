62086387bf2e264292b8031f7e00a8957c1b1bc1
MystGunn <157536861+MystGunn@users.noreply.github.com>
2024-07-13 18:28:53 +0200

    Exercitiul 2
    Am terminat exercitiul 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const saveCurrentTime = throttle((currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+}, 1000);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+    player.setCurrentTime(savedTime).catch(function(error) {
+        console.error('Error setting current time:', error);
+    });
+}
+
+player.on('timeupdate', saveCurrentTime);
